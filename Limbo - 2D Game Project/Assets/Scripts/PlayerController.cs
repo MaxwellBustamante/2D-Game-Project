2cb5f292768b1a0e801b932de6ca0a995297f8db
Eddy Elguezabal <eddye0820@gmail.com>
2021-02-15 23:25:07 -0500

    Basic Player Controller

---
+++ Limbo - 2D Game Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private float movementInputDirection;
+
+    private bool isGrounded;
+    private bool canJump;
+
+    public float movementSpeed = 10.0f;
+    public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        CheckInput();
+        CheckIfCanJump();
+    }
+
+    private void FixedUpdate()
+    {
+       ApplyMovement(); 
+       CheckSurroundings();
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirection = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded)// && rigidBody.velocity.y <= 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        rigidBody.velocity = new Vector2(movementSpeed * movementInputDirection, rigidBody.velocity.y);
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void Jump()
+    {   
+        if(canJump)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}

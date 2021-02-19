17406fb564001f75b6755ea2f4cf9a561ffc5de8
Eddy Elguezabal <eddye0820@gmail.com>
2021-02-19 03:48:52 -0500

    Basic climbing implementation/asset organization

--- Limbo - 2D Game Project/Assets/Scripts/PlayerController.cs
+++ Limbo - 2D Game Project/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rigidBody;
     private float movementInputDirection;
+    private float verticalInputDirection;
 
     private bool isGrounded;
+    private bool isTouchingPipe;
     private bool canJump;
+    private bool canClimb;
+    private bool isFacingLeft = true;
 
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
+    public float climbingSpeed = 10.0f;
     public float groundCheckRadius;
+    public float pipeCheckRadius;
 
     public Transform groundCheck;
+    public Transform pipeCheck;
 
     public LayerMask whatIsGround;
+    public LayerMask whatIsPipe;
 
     void Start()
     {
@@ -27,6 +35,8 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         CheckIfCanJump();
+        CheckIfCanClimb();
+        CheckMovementDirection();
     }
 
     private void FixedUpdate()
@@ -34,26 +44,59 @@ public class PlayerController : MonoBehaviour
        ApplyMovement(); 
        CheckSurroundings();
     }
+    private void CheckMovementDirection()
+    {
+        if(isFacingLeft && movementInputDirection > 0)
+        {
+            Flip();
+        }
+        else if(!isFacingLeft && movementInputDirection < 0)
+        {
+            Flip();
+        }
+    }
 
     private void CheckInput()
     {
         movementInputDirection = Input.GetAxisRaw("Horizontal");
+        verticalInputDirection = Input.GetAxisRaw("Vertical");
 
         if(Input.GetButtonDown("Jump"))
         {
-            Jump();
+            if(!canClimb)
+            {
+                Jump();
+            }       
+        }
+        if(Input.GetButtonDown("Vertical"))
+        {
+            Climb();
         }
     }
 
     private void CheckIfCanJump()
     {
-        if(isGrounded)// && rigidBody.velocity.y <= 0)
+        if(isGrounded)
         {
             canJump = true;
         }
         else
         {
             canJump = false;
+        }
+    }
+
+    private void CheckIfCanClimb()
+    {
+        if(isTouchingPipe)
+        {
+            canClimb = true;
+            rigidBody.bodyType = RigidbodyType2D.Kinematic;
+        }
+        else
+        {
+            canClimb = false;
+            rigidBody.bodyType = RigidbodyType2D.Dynamic;
         }
     }
 
@@ -65,6 +108,7 @@ public class PlayerController : MonoBehaviour
     private void CheckSurroundings()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        isTouchingPipe = Physics2D.OverlapCircle(pipeCheck.position, pipeCheckRadius, whatIsPipe);
     }
 
     private void Jump()
@@ -72,12 +116,27 @@ public class PlayerController : MonoBehaviour
         if(canJump)
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce);
+        }  
+    }
+
+    private void Climb()
+    {
+        if(canClimb)
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, climbingSpeed * verticalInputDirection);     
         }
         
+    }
+
+    private void Flip()
+    {
+        isFacingLeft = !isFacingLeft;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        Gizmos.DrawWireSphere(pipeCheck.position, pipeCheckRadius);
     }
 }

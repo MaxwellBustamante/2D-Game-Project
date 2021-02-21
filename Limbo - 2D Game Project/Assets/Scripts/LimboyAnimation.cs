a0e702ce7ccd404158f5cb17731fd940e2ac64b0
MaxwellBustamante <59800680+MaxwellBustamante@users.noreply.github.com>
2021-02-20 22:31:58 -0500

    Bunch of stuff
    Stuff includes:
    Title Art. Title Font
    Sounds and music to use
    Animations, animation controller and animation script

---
+++ Limbo - 2D Game Project/Assets/Scripts/LimboyAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimboyAnimation : MonoBehaviour
+{
+    public Animator PlayerAnimator;
+    public string WalkTrigger = "SetWalk", JumpTrigger = "SetJump", ClimbTrigger = "SetClimb";
+
+    public void SetAnimationTrigger(string TriggerName)
+    {
+        PlayerAnimator.SetTrigger(TriggerName);
+    }
+
+    void Start()
+    {
+        if (PlayerAnimator == null)
+        {
+            PlayerAnimator = GetComponent<Animator>();
+        } 
+    }
+
+    [ContextMenu("TestWalk")]
+    public void SetWalk()
+    {
+        SetAnimationTrigger(WalkTrigger);
+    }
+
+    [ContextMenu("TestJump")]
+    public void SetJump()
+    {
+        SetAnimationTrigger(JumpTrigger);
+    }
+
+    [ContextMenu("TestClimb")]
+    public void SetClimb()
+    {
+        SetAnimationTrigger(ClimbTrigger);
+    }
+
+    void Update()
+    {
+        
+    }
+}
